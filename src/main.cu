7f5d4adb5833d34273fc64980a3113fcdbc22545
katznboyz1 <harrison.lindgren@gmail.com>
2022-01-09 14:58:02 -0500

    main.cu now reads the config file

--- src/main.cu
+++ src/main.cu
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
 
 using namespace std;
 
@@ -12,7 +13,12 @@ std::string WEB_ROOT_DIR = "/dev/null";
 
 void readConfig(std::string configPath) {
 
+    boost::property_tree::ptree loadedPTreeRoot;
+    boost::property_tree::read_json(CONFIG_PATH, loadedPTreeRoot);
 
+    MAX_KERNELS = loadedPTreeRoot.get_child("threads").get_value<uint32_t>();
+    OUTGOING_PORT_ADDR = loadedPTreeRoot.get_child("port").get_value<uint16_t>();
+    WEB_ROOT_DIR = loadedPTreeRoot.get_child("web_root").get_value<std::string>();
 }
 
 int main() {
@@ -22,7 +28,7 @@ int main() {
     // read the config file
     readConfig(CONFIG_PATH);
 
-    std::cout << "[main.cu] STARTING SERVER ON PORT " << std::to_string(OUTGOING_PORT_ADDR) << " WITH " << std::to_string(MAX_KERNELS) << "THREADS." << endl;
+    std::cout << "[main.cu] STARTING SERVER ON PORT " << std::to_string(OUTGOING_PORT_ADDR) << " WITH " << std::to_string(MAX_KERNELS) << " THREADS." << endl;
 
     // spawn the kernels
 
